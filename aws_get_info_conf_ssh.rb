635100a9d48952554e4cae2c6455d440eb4b2441
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2018-03-16 15:59:31 +0200

    Add comments

--- aws_get_info_conf_ssh.rb
+++ aws_get_info_conf_ssh.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/ruby
 
+#define variables
 home = "/root"
 regions = Array.new
 hosts_list = Array.new
 
+#searching and saving information about aws regions from ~/.aws/config
 File.open("#{home}/.aws/config").each { |line| regions << line[9..-2] if line.match("region") }
 
+#searching and saving information about ec2 instances from aws
 regions.each { |i| hosts_list.concat(`aws ec2 describe-instances --region #{i} --query 'Reservations[*].Instances[*].[PublicDnsName,Tags[?Key==\`Name\`].Value]' --filters "Name=instance-state-name,Values=running"`.split("\"").grep(/^[\w]/)) }
 
+#save information about ec2 instances to ~/.ssh/config
 i = 0
 while i <= hosts_list.size()/2
   out_file = File.new("#{home}/.ssh/config", "a")
