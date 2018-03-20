ae55fab32b59581074f45476f7b083d2bee7c635
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2018-03-20 10:36:09 +0200

    Add ELB

--- create_ec2_instance_aws.rb
+++ create_ec2_instance_aws.rb
@@ -2,6 +2,7 @@
 
 require 'aws-sdk-ec2'
 require 'base64'
+require 'aws-sdk-elasticloadbalancing'
 
 az = 'us-east-1a'
 vpc_net = '172.31.0.0/16'
@@ -91,8 +92,35 @@ while count > 0
       delete_on_termination: true,
       associate_public_ip_address: true}]
   })
+  
 #Wait for the instance to be created, running, and passed status checks
 ec2.client.wait_until(:instance_status_ok, {instance_ids: [instance.first.id]})
 tag(instance, "MyInstance#{count}")
 count -= 1
+end
+
+#Creating ELB
+elb = Aws::ElasticLoadBalancing::Client.new(region: 'us-east-1')
+elb.create_load_balancer({
+  listeners: [
+    {
+      instance_port: 80,
+      instance_protocol: "HTTP",
+      load_balancer_port: 80,
+      protocol: "HTTP",
+    },
+  ],
+  load_balancer_name: "MyLoadBalancer",
+  security_groups: [sg.id],
+  subnets: [subnet.id],
+})
+
+#Registering ec2 instances to ELB
+ec2.instances.each do |instance|
+  if instance.state.name == "running"
+    elb.register_instances_with_load_balancer({
+      instances: [{ instance_id: "#{instance.id}"}],
+      load_balancer_name: "MyLoadBalancer"
+    })
+  end
 end
